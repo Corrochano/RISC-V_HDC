d403aa83059ad9e92e7ef4127832b9f1799a0f45
Álvaro Corrochano López <42788636+Corrochano@users.noreply.github.com>
2026-06-23 17:05:15 +0200

    Refactor

--- BasicHDCKernels/scalarKernels.cpp
+++ BasicHDCKernels/scalarKernels.cpp
@@ -13,9 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#pragma once
+#include "scalarKernels.h"
 
-#include "typeDefine.h"
+using namespace std;
 
 //////////////////
 // Bind Section //
@@ -25,25 +25,45 @@ void scalar_bind(
     const hdc_word_t *x,
     const hdc_word_t *y,
     hdc_word_t *z,
-    size_t words);
+    size_t words) 
+{    
+        for (size_t i = 0; i < words; ++i) {
+        z[i] = x[i] ^ y[i];
+    }
+}
 
 void scalar_bind(
     const hdc_word_t_32 *x,
     const hdc_word_t_32 *y,
     hdc_word_t_32 *z,
-    size_t words);
+    size_t words) 
+{    
+        for (size_t i = 0; i < words; ++i) {
+        z[i] = x[i] ^ y[i];
+    }
+}
 
 void scalar_bind(
     const hdc_word_t_16 *x,
     const hdc_word_t_16 *y,
     hdc_word_t_16 *z,
-    size_t words);
+    size_t words) 
+{    
+        for (size_t i = 0; i < words; ++i) {
+        z[i] = x[i] ^ y[i];
+    }
+}
 
 void scalar_bind(
     const hdc_word_t_8 *x,
     const hdc_word_t_8 *y,
     hdc_word_t_8 *z,
-    size_t words);
+    size_t words) 
+{    
+        for (size_t i = 0; i < words; ++i) {
+        z[i] = x[i] ^ y[i];
+    }
+}
 
 /////////////////////
 // Hamming Section //
@@ -53,25 +73,57 @@ void scalar_hamming(
     const hdc_word_t *x,
     const hdc_word_t *y,
     hdc_score_t *acc,
-    size_t words);
+    size_t words)
+{
+
+    for (size_t i = 0; i < words; ++i) {
+        if ((x[i] ^ y[i]) != 0) {
+            (*acc)++;
+        }
+    }
+}
 
 void scalar_hamming(
     const hdc_word_t_32 *x,
     const hdc_word_t_32 *y,
     hdc_score_t_32 *acc,
-    size_t words);
+    size_t words)
+{
+
+    for (size_t i = 0; i < words; ++i) {
+        if ((x[i] ^ y[i]) != 0) {
+            (*acc)++;
+        }
+    }
+}
 
 void scalar_hamming(
     const hdc_word_t_16 *x,
     const hdc_word_t_16 *y,
     hdc_score_t_16 *acc,
-    size_t words);
+    size_t words)
+{
+
+    for (size_t i = 0; i < words; ++i) {
+        if ((x[i] ^ y[i]) != 0) {
+            (*acc)++;
+        }
+    }
+}
 
 void scalar_hamming(
     const hdc_word_t_8 *x,
     const hdc_word_t_8 *y,
     hdc_score_t_8 *acc,
-    size_t words);
+    size_t words)
+{
+
+    for (size_t i = 0; i < words; ++i) {
+        if ((x[i] ^ y[i]) != 0) {
+            (*acc)++;
+        }
+    }
+}
 
 ///////////////////
 // Query Section //
@@ -82,25 +134,49 @@ void scalar_query(
     const hdc_word_t *q,
     hdc_score_t *scores,
     size_t nvec,
-    size_t words);
+    size_t words)
+{
+for (size_t i = 0; i < nvec; ++i) {
+        scores[i] = 0; 
+        scalar_hamming(&M[i * words], q, &scores[i], words);
+    }
+}
 
 void scalar_query( 
     const hdc_word_t_32 *M,
     const hdc_word_t_32 *q,
     hdc_score_t_32 *scores,
     size_t nvec,
-    size_t words);
+    size_t words)
+{
+for (size_t i = 0; i < nvec; ++i) {
+        scores[i] = 0; 
+        scalar_hamming(&M[i * words], q, &scores[i], words);
+    }
+}
 
 void scalar_query( 
     const hdc_word_t_16 *M,
     const hdc_word_t_16 *q,
     hdc_score_t_16 *scores,
     size_t nvec,
-    size_t words);
+    size_t words)
+{
+for (size_t i = 0; i < nvec; ++i) {
+        scores[i] = 0; 
+        scalar_hamming(&M[i * words], q, &scores[i], words);
+    }
+}
 
 void scalar_query( 
     const hdc_word_t_8 *M,
     const hdc_word_t_8 *q,
     hdc_score_t_8 *scores,
     size_t nvec,
-    size_t words);
+    size_t words)
+{
+for (size_t i = 0; i < nvec; ++i) {
+        scores[i] = 0; 
+        scalar_hamming(&M[i * words], q, &scores[i], words);
+    }
+}
